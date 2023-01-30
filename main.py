d92aa178bd9f7ba0cdc38e8174a12f59f91605d0
Lucas Freitas <lucasmatheus.profissional@gmail.com>
2023-01-30 11:43:19 -0300

    Add foms flask_wtf

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "superkey191919"
+
+
+# Create a Form Class
+class NameForm(FlaskForm):
+    name =  StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 
 @app.route('/')
 def index():
@@ -12,6 +26,20 @@ def index():
 def profile(name):
     return render_template("profile.html", title="Profile", name=name)
 
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = NameForm()
+    # Validate Form
+    if form.validate_on_submit():
+       name = form.name.data
+       form.name.data = '' 
+
+    return render_template('name.html',
+    name = name,
+    form = form)
+
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
@@ -19,6 +47,7 @@ def not_found(e):
 @app.errorhandler(500)
 def internal_error(e):
     return render_template("500.html"), 500
+
 
 
 if __name__ == '__main__':
